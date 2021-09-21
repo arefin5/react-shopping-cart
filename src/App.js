558a112319b577cf3b4e6cb5900f9fa9f28383e5
arefin5 <arefintalukder5@gmail.com>
2021-09-21 19:22:04 +0600

    create product component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 //feature-1
-const  App=()=> {
-  return (
-    <div className="Gried-container">
-     
+
+import React, { Component } from 'react';
+import Products from './components/Products';
+import data from './data.json'
+class App extends Component {
+  state={
+    products:data.products ,
+    size:"" ,
+    sort:""
+  }
+  render() {
+    return (
+      <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>
+        <div className="content">
+          <div className="main">
+             <Products Products={this.state.products}/>
+          </div>
+          <div className="sidebar">Cart Items</div>
+        </div>
+      </main>
+      <footer>All right is reserved.</footer>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
+
---
+++ src/util.js
@@ -0,0 +1,1 @@
+ export const formatCurrency=num=>"$" + Number(num.toFixed(1)).toLocaleString() + "" ;
